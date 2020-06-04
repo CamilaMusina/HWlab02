3d95aeef909a58fc5145ceea3a2c964e5092dae4
CamilaMusina <56271888+CamilaMusina@users.noreply.github.com>
2020-06-04 16:14:12 +0300

    Update Hello world.cpp

--- Hello world.cpp
+++ Hello world.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 int main(int argc, char** argv){
- std::string name; //user name;
- std::cin >> name; //ввод данных
+ std::string name; //user name
+ std::cin >> name; //input user name
  std::cout << "Hello world from " << name << std::endl;
 }
